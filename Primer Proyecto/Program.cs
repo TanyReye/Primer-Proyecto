65064f7f728f2b5434987efb05c1b81b371f2972
Tany <Tany@DESKTOP-6BBEID0>
2026-03-24 17:29:21 -0600

    Menu parte 1

--- Primer Proyecto/Program.cs
+++ Primer Proyecto/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int menu = 0;
+
+int total = 0;
+int publicados = 0;
+int rechazados = 0;
+int revision = 0;
+
+int impactoAlto = 0;
+int impactoMedio = 0;
+int impactoBajo = 0;
+
+do
+{
+
+    Console.WriteLine(" ");
+    Console.WriteLine("Selecciona una opción en número: ");
+    Console.WriteLine("1. Evaluar nuevo contenido. ");
+    Console.WriteLine("2. Mostrar reglas del sistema. ");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión. ");
+    Console.WriteLine("4. Reiniciar estadísticas. ");
+    Console.WriteLine("5. Salir. ");
+    Console.WriteLine(" ");
